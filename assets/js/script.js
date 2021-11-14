e66802487f279dee72565a6cd0d50fae3ea008c1
sarahwesley <92330391+sarahwesley@users.noreply.github.com>
2021-11-14 09:19:25 -0500

    dom elements added

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// DOM elements
+var startButtonEl = document.querySelector('#start');
+var questionEl = document.querySelector('#questions');
+var answerEl = document.querySelector('#answers');
+var startPageEl =  document.querySelector('#quizpage');
+var scorePageEl = document.querySelector('#score');
+var questionConEl = document.querySelector('#questionsContainer');
+var correctEl = document.querySelector('#prompt');
+var countdownEl = document.querySelector('#countdown');
+var scoreAreaEl = document.querySelector('#scoreArea');
+var inNameEl = document.querySelector('#inName');
+var buttonDivEl = document.querySelector('#saveBtn');
+var highScoreEl = document.querySelector('#highScores');
